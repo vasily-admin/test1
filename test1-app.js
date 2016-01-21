03b757bf725affb86bb0543c114bbcb111b14cec
vasily-admin <vasily@consigo.ca>
2016-01-21 00:16:09 -0500

    Update test1-app.js

--- test1-app.js
+++ test1-app.js
@@ -1,10 +1,12 @@
-const http = require('http');
-const hostname = 'localhost';
-const port = 8000;
+//const http = require('http');
+//const hostname = 'localhost';
+//const port = 8000;
 
-http.createServer((req, res) => {
+/*http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
   res.end('Hello World\n');
 }).listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
-});
+});*/
+
+console.log('Vasya - privet');
